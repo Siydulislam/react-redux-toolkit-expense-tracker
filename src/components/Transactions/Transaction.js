e04e32a89670e05c29856ca9974f9458755c6d1d
Siydulislam <siydulislam333@gmail.com>
2022-09-28 23:32:51 +0600

    calculate final balance feature

--- src/components/Transactions/Transaction.js
+++ src/components/Transactions/Transaction.js
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import deleteImage from "../../assets/images/delete.svg";
 import editImage from "../../assets/images/edit.svg";
 import { editActive, removeTransaction } from "../../features/transaction/transactionSlice";
+import numberWithCommas from "../../utils/numberWithCommas";
 
 export default function Transaction({ transaction }) {
     const { name, amount, type, id } = transaction || {};
@@ -19,7 +20,7 @@ export default function Transaction({ transaction }) {
         <li className={`transaction ${type}`}>
             <p>{name}</p>
             <div className="right">
-                <p>৳ {amount}</p>
+                <p>৳ {numberWithCommas(amount)}</p>
                 <button className="link" onClick={handleEdit}>
                     <img alt="Edit" className="icon" src={editImage} />
                 </button>
